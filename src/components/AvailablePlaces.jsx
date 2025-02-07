8fb00ba0bbf19b54fe985eebeea56d940773c7bc
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-02-07 17:54:03 -0600

    Added Custom Error

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,23 +1,44 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import CustomError from "./CustomError.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
         setAvailablePlaces(resData.places);
-      });
+      } catch (error) {
+        setError(error);
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
   }, []);
+
+  if (error) {
+    return <CustomError title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
